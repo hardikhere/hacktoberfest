1c0a56e60e9871313d42f28381d71629b7b9ce4b
hardik khanchandani <51117080+hardikhere@users.noreply.github.com>
2019-10-09 12:06:58 +0530

    Create Matrix-manipulation.cpp

---
+++ Matrix-manipulation.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+	int t;
+	int x,y;
+	cin>>t;
+	while(t--)
+	{
+	int r,c,q,cnt=0;
+	cin>>r>>c>>q;
+	int a[r][c]={0};
+	int *p;
+	p=&a[0][0];
+	for(int i=0;i<c;i++)
+	a[0][i]=0;
+	while(q--)
+	{
+	cin>>x>>y;
+	x--;
+	y--;
+	#include<iostream>
+using namespace std;
+int  main()
+{
+	int t;
+	int x,y;
+	cin>>t;
+	while(t--)
+	{
+	int r,c,q,cnt=0;
+	cin>>r>>c>>q;
+	int a[r][c]={0};
+	int *p;
+	p=&a[0][0];
+	for(int i=0;i<c;i++)
+	a[0][i]=0;
+	while(q--)
+	{
+	cin>>x>>y;
+	x--;
+	y--;
+
+	}
+
+		for(int i=0;i<r*c;i++)
+	    {
+	    	int item;
+	    	item=*p;
+	    	if(item%2!=0)
+	    	cnt++;
+	    	p++;
+	    }
+	cout<<cnt<<endl;	
+	}
+}
+	}
+
+		for(int i=0;i<r*c;i++)
+	    {
+	    	int item;
+	    	item=*p;
+	    	if(item%2!=0)
+	    	cnt++;
+	    	p++;
+	    }
+	cout<<cnt<<endl;	
+	}
+}
