276f8c734eb3d34f6fc01df9b09d4caf2f4f2493
Divya S <divyas1301@gmail.com>
2019-10-14 20:46:11 +0530

    made some changes, hope this helps

--- Matrix-manipulation.cpp
+++ Matrix-manipulation.cpp
@@ -1,69 +1,52 @@
 #include<iostream>
+#include<iomanip>
+#include<stdlib.h>
+
 using namespace std;
 int  main()
 {
-	int t;
-	int x,y;
-	cin>>t;
-	while(t--)
-	{
-	int r,c,q,cnt=0;
-	cin>>r>>c>>q;
-	int a[r][c]={0};
-	int *p;
-	p=&a[0][0];
-	for(int i=0;i<c;i++)
-	a[0][i]=0;
-	while(q--)
-	{
-	cin>>x>>y;
-	x--;
-	y--;
-	#include<iostream>
-using namespace std;
-int  main()
-{
-	int t;
-	int x,y;
-	cin>>t;
-	while(t--)
-	{
-	int r,c,q,cnt=0;
-	cin>>r>>c>>q;
-	int a[r][c]={0};
-	int *p;
-	p=&a[0][0];
-	for(int i=0;i<c;i++)
-	a[0][i]=0;
-	while(q--)
-	{
-	cin>>x>>y;
-	x--;
-	y--;
+    int t;
+    int x,y;
+    int r,c,q,cnt=0;
+    int *p;
+    cin>>t;
+    while(t--)
+    {
+        cin>>r>>c>>q;
+        int a[r][c]={0};
+        p=&a[0][0];
 
-	}
+        for(int i=0;i<c;i++)
+            a[0][i]=0;
 
-		for(int i=0;i<r*c;i++)
-	    {
-	    	int item;
-	    	item=*p;
-	    	if(item%2!=0)
-	    	cnt++;
-	    	p++;
-	    }
-	cout<<cnt<<endl;	
-	}
+        while(q--)
+        {
+            cin>>x>>y;
+            x--;
+            y--;
+        }
+
+        for(int i=0;i<r*c;i++)
+        {
+            int item;
+            item=*p;
+            if(item%2!=0)
+            cnt++;
+            p++;
+        }
+
+        cout<<cnt<<endl;    
+    }
+
+    for(int i=0;i<r*c;i++)
+    {
+        int item;
+        item=*p;
+        if(item%2!=0)
+        cnt++;
+        p++;
+    }
+
+    cout<<cnt<<endl;    
 }
-	}
 
-		for(int i=0;i<r*c;i++)
-	    {
-	    	int item;
-	    	item=*p;
-	    	if(item%2!=0)
-	    	cnt++;
-	    	p++;
-	    }
-	cout<<cnt<<endl;	
-	}
-}
